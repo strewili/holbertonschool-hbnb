35509db9aaff963722b869d42baf147fe51d6fce
kayanabdullah00-eng <kayanabdullah00@gmail.com>
2026-07-28 12:22:11 +0300

    add part3

---
+++ part3/app/auth.py
@@ -0,0 +1,27 @@
+from flask_restx import Namespace, Resource, fields
+from flask_jwt_extended import create_access_token
+from app.services import facade
+
+api = Namespace('auth', description='Authentication operations')
+
+login_model = api.model('Login', {
+    'email': fields.String(required=True),
+    'password': fields.String(required=True)
+})
+
+@api.route('/login')
+class Login(Resource):
+    @api.expect(login_model)
+    def post(self):
+        credentials = api.payload
+        user = facade.get_user_by_email(credentials['email'])
+
+        if not user or not user.verify_password(credentials['password']):
+            return {'error': 'Invalid credentials'}, 401
+
+        additional_claims = {"is_admin": user.is_admin}
+        access_token = create_access_token(
+            identity=user.id,
+            additional_claims=additional_claims
+        )
+        return {'access_token': access_token}, 200
